faf57fd132f2728035149c936bf51c62b63f7065
sushi-chaaaan <sushi_code@outlook.jp>
2022-12-03 15:38:02 +0900

    publish 0.1.0, set icon toggle

--- src/background.js
+++ src/background.js
@@ -5,6 +5,8 @@
 // For more information on background script,
 // See https://developer.chrome.com/extensions/background_pages
 
+let available = false
+
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if (request.type === "GREETINGS") {
     const message = `Hi ${
@@ -24,15 +26,14 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
   if (changeInfo.status === "complete") {
     console.log(`got url: ${tab.url}`)
-    const filter = generate_filter()
-    const check_pattern = generate_filter(true)
-
     // 処理
     if (
       tab.url.match(/https?:\/\/www.youtube.com\/results?/) &&
-      !tab.url.includes(check_pattern)
+      !tab.url.includes(generate_filter(true)) &&
+      available
     ) {
       console.log("url matched. adding filter")
+      const filter = generate_filter()
       const new_url = tab.url + "+" + filter
       chrome.tabs.update(tabId, { url: new_url })
     }
@@ -51,3 +52,18 @@ function generate_filter(regex = false) {
   console.log(`filter: ${filter}, regex: ${regex}`)
   return filter
 }
+
+// eslint-disable-next-line no-unused-vars
+chrome.action.onClicked.addListener((tab) => {
+  available = !available
+
+  if (available) {
+    chrome.action.setIcon({
+      path: "icons/search_on.png",
+    })
+  } else {
+    chrome.action.setIcon({
+      path: "icons/search_off.png",
+    })
+  }
+})
